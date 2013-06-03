b7cfe5c052a762dd6e0e9e367f3088f06f6e7b0e
imeinzen <imeinzen@DYN.DATASYS.SWRI.EDU>
2013-06-03 05:26:02 +0000

    Updated Exercise 1.6

--- training/ref/1.6/lesson_simple_topic/simple_publisher.cpp
+++ training/ref/1.6/lesson_simple_topic/simple_publisher.cpp
@@ -2,7 +2,7 @@
  ** Simple ROS Publisher Node
  **/
 #include <ros/ros.h>
-#include <lesson_simple_topic/PathPosition.h>
+#include <lesson_simple_topic/PathPosition.h> // TODO: Uncomment this line
 
 
 int main(int argc, char* argv[])
@@ -14,14 +14,15 @@ int main(int argc, char* argv[])
     ros::Rate loop_rate(1.0);
 
     // Advertise that we're publishing the topic "position", of type PathPosition
-    ros::Publisher pub = node.advertise<lesson_simple_topic::PathPosition>("position", 1000);
+    ros::Publisher pub = node.advertise<lesson_simple_topic::PathPosition>("position", 1000); // TODO: Uncomment this line
     
     // The angle counter
-    int angle = 0;
+    int angle = 0; 
 
     ROS_INFO("Starting publisher");
     while(ros::ok()) {
-        // Create a message, and set the fields appropriately
+        // Create a message, and set the fields appropriately 
+        // TODO: Uncomment the next 4 lines
         lesson_simple_topic::PathPosition msg;
         msg.angle = angle * 3.141592 / 180.0;
         msg.x = 100.0 * cos(msg.angle);
@@ -30,6 +31,7 @@ int main(int argc, char* argv[])
         angle = (angle + 10) % 360;
 
         // Publish the message, give ROS an opportunity to run
+        // TODO: Uncomment the next 3 lines
         pub.publish(msg);
         ros::spinOnce();
         ROS_INFO("Published message %.1f, %.1f, %.1f", msg.x, msg.y, msg.angle * 180.0 / 3.141592);
--- training/ref/1.6/lesson_simple_topic/simple_subscriber.cpp
+++ training/ref/1.6/lesson_simple_topic/simple_subscriber.cpp
@@ -2,8 +2,9 @@
  ** Simple ROS Subscriber Node
  **/
 #include <ros/ros.h>
-#include <lesson_simple_topic/PathPosition.h>
+#include <lesson_simple_topic/PathPosition.h> // TODO: Uncomment this line
 
+// TODO: Uncomment the next 5 lines
 void positionCallback(const lesson_simple_topic::PathPosition& msg)
 {
     ROS_INFO("New position: %.1f,%.1f,%.1f", msg.x, msg.y, 
@@ -15,6 +16,7 @@ int main(int argc, char* argv[])
     ros::init(argc, argv, "simple_subscriber");
     ros::NodeHandle node;
 
+    // TODO: Uncomment the next 2 lines
     ros::Subscriber subscriber = node.subscribe("position", 1000, positionCallback);
     ros::spin();
     return 0;
