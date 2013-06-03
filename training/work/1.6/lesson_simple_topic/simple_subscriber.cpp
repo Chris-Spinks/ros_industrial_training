ea768e7312de8fede1fbd52d1c5d8e10764ad654
imeinzen <imeinzen@DYN.DATASYS.SWRI.EDU>
2013-06-03 12:37:17 +0000

    Fixes to remove work done in "work" folder.

--- training/work/1.6/lesson_simple_topic/simple_subscriber.cpp
+++ training/work/1.6/lesson_simple_topic/simple_subscriber.cpp
@@ -2,14 +2,14 @@
  ** Simple ROS Subscriber Node
  **/
 #include <ros/ros.h>
-#include <lesson_simple_topic/PathPosition.h> // TODO: Uncomment this line
+//#include <lesson_simple_topic/PathPosition.h> // TODO: Uncomment this line
 
 // TODO: Uncomment the next 5 lines
-void positionCallback(const lesson_simple_topic::PathPosition& msg)
-{
-    ROS_INFO("New position: %.1f,%.1f,%.1f", msg.x, msg.y, 
-        msg.angle * 180.0 / 3.141592);
-}
+//void positionCallback(const lesson_simple_topic::PathPosition& msg)
+//{
+//    ROS_INFO("New position: %.1f,%.1f,%.1f", msg.x, msg.y, 
+//        msg.angle * 180.0 / 3.141592);
+//}
 
 int main(int argc, char* argv[])
 {
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
     ros::NodeHandle node;
 
     // TODO: Uncomment the next 2 lines
-    ros::Subscriber subscriber = node.subscribe("position", 1000, positionCallback);
+//   ros::Subscriber subscriber = node.subscribe("position", 1000, positionCallback);
     ros::spin();
     return 0;
 }
