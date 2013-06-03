e453d6470a5a2779c1d425406e88855cdee24773
imeinzen <imeinzen@DYN.DATASYS.SWRI.EDU>
2013-06-03 04:34:53 +0000

    Updated Exercise 1.5

--- training/ref/1.5/lesson_simple_parameters/simple_parameters.cpp
+++ training/ref/1.5/lesson_simple_parameters/simple_parameters.cpp
@@ -2,17 +2,18 @@
  ** Simple ROS Node
  **/
 #include <ros/ros.h>
-#include <string>
+#include <iostream>
 
 
 int main(int argc, char* argv[])
 {
     // This must be called before anything else ROS-related 
-    ros::init(argc, argv, "simple_node");
+    ros::init(argc, argv, "simple_parameters");
 
-    // Create ROS node handles
-    ros::NodeHandle node;
-    ros::NodeHandle local_node("~");
+    // Create a ROS node handle
+    ros::NodeHandle node("~");
+    //ros::NodeHandle node("/simple_parameters");   // STEP 2: Change 'node;' to 'node("/simple_parameters");'
+    //ros::NodeHandle node("~");                    // STEP 3: Change 'node("/simple_parameters")' to 'node("~");'
 
     // Set the rate at which we print out our message (1Hz)
     ros::Rate loop_rate(1.0);
@@ -22,34 +23,28 @@ int main(int argc, char* argv[])
 
     // Loop through until the ROS system tells the user to shut down
     while(ros::ok()) {
-        // Print out a message
-        ROS_INFO("We've gone through %d times", count);
+        // Print out the current iteration
+        ROS_INFO_STREAM("Iteration number " << count << ": ");
         ++count;
-        
-        // --== ADD NEW LINES HERE... ==-- //
-        // Print out the value we've read, if it has been set
-        int value_int;
-        if(!node.hasParam("integer")) {
-            ROS_WARN("Integer parameter is not set.");
-        } else if(node.getParam("integer", value_int)) {
-            ROS_INFO("Integer parameter is currently %d.", value_int);
+
+        // STEP 1: Uncomment the following 8 lines to read out integer parameter
+        int integer_value;
+        if(!node.hasParam("integer_value")) {
+            ROS_WARN_STREAM("  integer_value is not set");
+        } else if(!node.getParam("integer_value", integer_value)) {
+            ROS_ERROR_STREAM("  integer_value is wrong type");
         } else {
-            ROS_ERROR("Integer parameter is not an integer.");
+            ROS_INFO_STREAM("  integer_value is " << integer_value);
         }
 
-        std::string value_string;
-        if(!node.hasParam("values/string")) {
-            ROS_WARN("String parameter is not set.");
-        } else if(node.getParam("values/string", value_string)) {
-            ROS_INFO("String parameter is currently '%s'.", value_string.c_str());
-        } else {
-            ROS_ERROR("String parameter is not a string."); 
-        }
-
+        // STEP 4: Uncomment the following 6 lines
         double x, y;
-        node.param("/values/point/x", x, 0.0);
-        node.param("/values/point/y", y, 0.0);
-        local_node.setParam("point_sum", x + y);
+        node.param("point/x", x, 0.0);
+        node.param("point/y", y, 0.0);
+        node.setParam("/global_sum", x + y);
+        ROS_INFO_STREAM("  Sum of point values " << x << " & " << y << ": " 
+                << x + y);
+
 
         // Wait the stated duration
         loop_rate.sleep();
